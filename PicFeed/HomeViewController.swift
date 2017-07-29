b1a4f8f2013b6ec959bface737fa2a03b21fd05c
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-07-29 10:54:38 -0700

    added camera option to image picker

--- PicFeed/HomeViewController.swift
+++ PicFeed/HomeViewController.swift
@@ -22,16 +22,38 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         presentAlertController()
     }
     
+    @IBAction func postButtonPressed(_ sender: Any) {
+        
+        if let newImage = self.selectedImageView.image {
+        
+            let newPost = Post(image: newImage)
+            
+            CloudKit.shared.save(post: newPost, completion: { (success) in
+                if success {
+                    print("Successfully Saved to the Cloud!")
+                } else {
+                    print("Unsuccessful in saving to CLoud...")
+                }
+            })
+        }
+    }
+    
+    
     func presentAlertController() {
         let alertController = UIAlertController(title: "Select Source", message: "Please select source for image", preferredStyle: .actionSheet)
         let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (action) in
             self.presentImagePicker(sourceType: .photoLibrary)
         }
         
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+            self.presentImagePicker(sourceType: .camera)
+        }
+        
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
         alertController.addAction(cancelAction)
         alertController.addAction(photoLibraryAction)
+        alertController.addAction(cameraAction)
         
         self.present(alertController, animated: true, completion: nil)
     }
