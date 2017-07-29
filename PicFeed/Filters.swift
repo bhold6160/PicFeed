0b5a446009f89fc26a750e6d863ce6d5a2a95f1b
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-07-29 13:14:08 -0700

    added filters class

---
+++ PicFeed/Filters.swift
@@ -0,0 +1,13 @@
+//
+//  Filters.swift
+//  PicFeed
+//
+//  Created by Brandon Holderman on 7/29/17.
+//  Copyright © 2017 Brandon Holderman. All rights reserved.
+//
+
+import UIKit
+
+class Filter {
+    var filters: [Filter]
+}
